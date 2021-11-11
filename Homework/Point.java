123f0cec75d9d3df061020523667d2721462f2dd
royaboya <94084201+royaboya@users.noreply.github.com>
2021-11-10 21:01:46 -0500

    Point Class

---
+++ Homework/Point.java
@@ -0,0 +1,70 @@
+public class Point {
+    private int xCoor;
+    private int yCoor;
+    private static int numOfPoints;
+
+    public Point() {
+        xCoor = 0;
+        yCoor = 0;
+        numOfPoints++;
+    }
+
+    public Point(int x, int y) {
+        xCoor = x;
+        yCoor = y;
+        numOfPoints++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getNumOfPoints() {
+        return numOfPoints;
+    }
+
+    public int getxCoor() {
+        return xCoor;
+    }
+
+    public int getyCoor() {
+        return yCoor;
+    }
+
+    public void setxCoor(int x) {
+        xCoor = x;
+    }
+
+    public void setyCoor(int y) {
+        yCoor = y;
+    }
+
+    public void setCoor(int x, int y) {
+        xCoor = x;
+        yCoor = y;
+    }
+
+    public void goHome() {
+        xCoor = 0;
+        yCoor = 0;
+    }
+
+    public void move(int deltaX, int deltaY) {
+        xCoor += deltaX;
+        yCoor += deltaY;
+    }
+
+    public double distance(Point other) {
+        double changeX = xCoor - other.xCoor;
+        double changeY = yCoor - other.yCoor;
+        return Math.sqrt(Math.pow(changeX, 2) + Math.pow(changeY, 2));
+    }
+
+    public String toString() {
+        return String.format("(%1$s,%2$s)", xCoor, yCoor);
+    }
+
+    public boolean equals(Point other) {
+        return xCoor == other.xCoor && yCoor == other.yCoor;
+    }
+}
---
+++ Homework/PointTester.java
@@ -0,0 +1,107 @@
+//testing class built by Huo Chen; Last Update 11/2/2021
+
+public class PointTester {
+    public static void main(String[] args) {
+        System.out.print("Constructing a Point called p1 using no-arges constructor...");
+        Point p1 = new Point();
+        System.out.println("PASSED");
+        System.out.println();
+
+        System.out.print("Constructing a Point called p2 using parameterized constructor...");
+        Point p2 = new Point(-3, -4);
+        System.out.println("PASSED");
+        System.out.println();
+
+        System.out.print("Checking p1 instant variables...");
+        if (p1.getxCoor() == 0 && p1.getyCoor() == 0) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking p2 instant variables...");
+        if (p2.getxCoor() == -3 && p2.getyCoor() == -4) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking static variable and method...");
+        if (Point.getNumOfPoints() == 2) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking setxCoor and setyCoor method...");
+        p1.setxCoor(1);
+        p1.setyCoor(2);
+        if (p1.getxCoor() == 1 && p1.getyCoor() == 2) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking setCoor method...");
+        p1.setCoor(3, 4);
+        if (p1.getxCoor() == 3 && p1.getyCoor() == 4) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking goHome method...");
+        p1.goHome();
+        if (p1.getxCoor() == 0 && p1.getyCoor() == 0) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking move method...");
+        p2.move(3, 4);
+        if (p2.getxCoor() == 0 && p2.getyCoor() == 0) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking distance method...");
+        Point p3 = new Point(3, 4);
+        if (p3.distance(p1) == 5) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+        }
+        System.out.println();
+
+        System.out.print("Checking toString method...");
+        if (p3.toString().equals("(3,4)")) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+            System.out.println("Expected output: (3,4)");
+            System.out.println("Your output: " + p3);
+        }
+        System.out.println();
+
+        System.out.print("Checking equals method...");
+        if (p1.equals(p2) && !p1.equals(p3)) {
+            System.out.println("PASSED");
+        } else {
+            System.out.println("FAILED!");
+            System.out.println("Expected output: (3,4)");
+            System.out.println("Your output: " + p3);
+        }
+        System.out.println();
+
+        System.out.println("END of TEST");
+    }
+}
