87dba7bc79a01f6a3ac7b7cfbb4f5d21cda4e638
royaboya <roychen1716@gmail.com>
2021-11-25 19:13:56 -0500

    updated AreaUnderCurve

--- Classwork/AreaUnderCurve.java
+++ Classwork/AreaUnderCurve.java
@@ -1,21 +1,24 @@
 package Classwork;
 
-// Riemann sum curve for x^2 assignment
+/** Brute force for Riemann sum assignment, area of f(x) = x^2 */
 public class AreaUnderCurve {
     public static void main(String[] args) {
-        System.out.println(xSquareArea(10, 0.000005));
+        // test values
+        System.out.println(xSquareArea(10, 0.0005));
+        System.out.println(xSquareArea(50, 0.0005));
+        System.out.println(xSquareArea(100, 0.0005));
     }
 
     public static double xSquareArea(double x1, double x2) {
         double horizontal = 0;
-        double sum = 0;
+        double areaSum = 0;
 
         for (double i = 0; i < x1; i += x2) {
             double currentHeight = Math.pow(horizontal, 2);
-            sum += currentHeight * x2;
+            areaSum += currentHeight * x2;
             horizontal += x2;
         }
-        return sum;
+        return areaSum;
 
     }
 }
