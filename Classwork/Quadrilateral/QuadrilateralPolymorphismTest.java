33732a941fe1a4fb1f4385ddaf087a3571a1e951
royaboya <roychen1716@gmail.com>
2022-01-10 12:39:36 -0500

    QuadrilateralPolymorphismTest CW

---
+++ Classwork/Quadrilateral/QuadrilateralPolymorphismTest.java
@@ -0,0 +1,33 @@
+package Classwork.Quadrilateral;
+
+public class QuadrilateralPolymorphismTest {
+    public static void main(String[] args) {
+        Quadrilateral q1 = new Quadrilateral();
+        Quadrilateral q2 = new Rectangle();
+        Quadrilateral q3 = new Square();
+
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Square();
+
+        // System.out.println(r1);
+        // System.out.println(r2);
+
+        System.out.println();
+        Square s1 = new Square();
+
+        // printObject(q1);
+        // printObject(q2);
+        // printObject(q3);
+
+        printObject(r1);
+        printObject(r2);
+
+        printObject(s1);
+
+    }
+
+    public static void printObject(Rectangle other) {
+        System.out.println(other);
+    }
+
+}
